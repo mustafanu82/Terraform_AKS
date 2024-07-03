353ec6b508269489764529475a8516fb3ab37164
Mohammad Mustafa <mustafanu82@outlook.com>
2024-07-03 12:17:44 +0000

    my first commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "backendrg"
-    storage_account_name = "backendtf001"
+    resource_group_name  = "backend-rg"
+    storage_account_name = "backendtf01"
     container_name       = "tfstate"
     key                  = "aks.terraform.tfstate"
   }
