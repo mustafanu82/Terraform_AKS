8208964342a837e27f90d94f427c0a7f429ef734
Mohammad Mustafa <mustafanu82@outlook.com>
2024-07-03 23:56:01 +0000

    version update

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  # 1. Required Version Terraform
+  required_version = ">= 0.13"
+  # 2. Required Terraform Providers  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
 provider "azurerm" {
   features {
 
