8671aae04d73bf8fc0d0d648b5e4c905622f0d6c
Mohammad Mustafa <mustafanu82@outlook.com>
2024-07-03 11:41:43 +0000

    rg name modified

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "backend-rg"
+    resource_group_name  = "backendrg"
     storage_account_name = "backendtf001"
     container_name       = "tfstate"
     key                  = "aks.terraform.tfstate"
